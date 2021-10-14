775362fe81cfa794ad36ee7bab2239953dfafee8
Jason Zhagui <jasonzhagui@gmail.com>
2021-10-14 01:55:47 -0400

    Beta v1

--- Platform Project/Assets/Code/Intermission ode/Asteroid.cs
+++ Platform Project/Assets/Code/Intermission ode/Asteroid.cs
@@ -8,45 +8,25 @@ public class Asteroid : MonoBehaviour
     float startX;
     float addY;
     float addX;
+    int random1;
+    int random2;
 
     // Start is called before the first frame update
     void Start()
     {
         startY = transform.position.y;
         startX = transform.position.x;
+        random1 = Random.Range(1, 4);
+        random2 = Random.Range(1, 4);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        addY = Mathf.PingPong(Time.time * 1, 2);
-        addX = Mathf.PingPong(Time.time * 1, 2);
+        print(random1);
+        addY = Mathf.PingPong(Time.time * 1, random1);
+        addX = Mathf.PingPong(Time.time * 1, random2);
         transform.position = new Vector3(transform.position.x, startY + addY, transform.position.z);
         transform.position = new Vector3(startX + addX, transform.position.y, transform.position.z);
     }
 }
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Move : MonoBehaviour
-{
-    public float speed = 2.5f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 5),transform.position.y,transform.position.z);
-        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, 3), transform.position.z);
-
-    }
-}
-*/
