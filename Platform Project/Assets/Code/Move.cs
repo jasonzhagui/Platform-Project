46186d6038e0b005cd8b509b1e59951ff45d03c5
Jason Zhagui <jasonzhagui@gmail.com>
2021-10-13 20:24:36 -0400

    Updated Intermission

---
+++ Platform Project/Assets/Code/Move.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Move : MonoBehaviour
+{
+  
+    float startY;
+    float startX;
+    float addY;
+    float addX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+        startX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        addY = Mathf.PingPong(Time.time * 1, 2);
+        addX = Mathf.PingPong(Time.time * 1, 2);
+        transform.position = new Vector3(transform.position.x, startY + addY, transform.position.z);
+        transform.position = new Vector3(startX + addX, transform.position.y, transform.position.z);
+    }
+}
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time * speed, 5),transform.position.y,transform.position.z);
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * speed, 3), transform.position.z);
+
+    }
+}
+*/
+
