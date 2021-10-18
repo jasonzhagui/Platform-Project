37adaeed45923ed2eece87c855072c3e92a2fbd6
Alice Khaing <ksy243@nyu.edu>
2021-10-18 15:35:17 -0400

    Add moving platforms

---
+++ Platform Project/Assets/Code/MovingUpDown.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingUpDown : MonoBehaviour
+{
+    float speed = .5f;
+    public float distance = 20;
+    float startY;
+    private void Start()
+    {
+        startY = transform.position.y;
+    }
+    private void Update()
+    {
+        Vector2 newPosition = transform.position;
+        newPosition.y = Mathf.SmoothStep(startY, startY + distance, Mathf.PingPong(Time.time * speed, 1));
+        transform.position = newPosition;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
